71500fc5fe69911c85993b3bcdac57d54482c07d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 16:28:18 -0800

    added printing digits exercise

---
+++ print-digits/print-digits.py
@@ -0,0 +1,35 @@
+"""Given int, print each digit in reverse order, starting with the ones place.
+
+For example::
+
+    >>> print_digits(1)
+    1
+
+    >>> print_digits(413)
+    3
+    1
+    4
+
+    >>> print_digits(7331)
+    1
+    3
+    3
+    7
+
+"""
+
+
+def print_digits(num):
+    """Given int, print digits in reverse order, starting with the ones place."""
+    if num == 0:
+        print 0
+    num = abs(num)
+    while num > 0:
+        print num % 10
+        num = num / 10
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. WOWZA!\n"
